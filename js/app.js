3c6c3c423fb485aca3d159423403c27032a008ea
Kathleen Rowland <kthlnrwlnd@yahoo.com>
2018-03-07 16:10:20 -0800

    created questions 2 to 4

--- js/app.js
+++ js/app.js
@@ -11,28 +11,56 @@
 alert("Star light, star bright, hope you ain't no Seattlite.");
 
 //first question
-var whereQuestion = (prompt('So you think I am from Seattle? Please answer "yes" or "no".')) .toLowerCase();
+var whereQuestion = (prompt('So, you think I am from Seattle? Answer "yes" or "no".')) .toLowerCase();
 
 if(whereQuestion === 'no') {
-  alert('User guessed wrong.' );
-  console.log('Wrong!');
+  alert('Wrong!.' );
+  console.log('Nope, I\'m from here.');
 } else {
   alert('What gave it away?');
-  console.log('Yes, I am a from Seattle.');
+  console.log('Yes, I am a Seattle native, but not indigenous.');
 }
 
 //second question
-var myUmbrella = prompt('Do I carry an umbrella? Please enter "yes" or "no"').toLowerCase();
+var umbrellaQuestion = prompt('Do we carry an umbrella? Answer "yes" or "no"').toLowerCase();
 
-
-// 'yes', 'YES', 'Y', 'y', YeS, NO, no, No, nO, n, N, nope, nah, yep
-
-if(myUmbrella.toLowerCase() === 'yes') {
-  alert('That is correct!');
+if(umbrellaQuestion.toLowerCase() === 'yes') {
+  alert('Nope!');
+  console.log('Real Seattle people don\'t carry umbrellas.' );
 } else {
-  alert('No, I am older than 30!');
+  alert('That\'s right. The dang things break or get lost.');
+  console.log('Real Seattle people don\'t carry umbrellas.');
 }
 //third question
+var hikeQuestion = (prompt('Do you like hiking? Answer "yes" or "no".')) .toLowerCase();
+
+if(hikeQuestion === 'no') {
+  alert('Why else would I live here?' );
+  console.log('People who live here have a religious attitude towards the mountains, especially Mount Rainer.');
+} else {
+  alert('I belong to REI.');
+  console.log('People who live here have a religious attitude towards the mountains, especially Mount Rainer.');
+
+}//fourth question
+var rainQuestion = (prompt('What they say about Seattle raining all the time is exaggerated.  Answer "yes" if you agree and "no" if you disagree.')) .toLowerCase();
+
+if(rainQuestion === 'no') {
+  alert('We like to keep that secret.' );
+  console.log('It just seems like it rains all the time here in Seattle.');
+} else {
+  alert('We like to keep that secret.');
+  console.log('It just seems like it rains all the time here in Seattle.');
+
+}//fifth question
+var icecreamQuestion = (prompt('Do we eat lots of ice cream? Please answer "yes" if this is a true statement and "no" if it is a false statement.')) .toLowerCase();
+
+if(icecreamQuestion === 'yes') {
+  alert('Great! Let\'s stop this game and go find some.' );
+  console.log('Seattle people are big coffee drinkers and icecream eaters.');
+} else {
+  alert('Well, I\'ll have to leave you now because I\'m headed to the store for some. ');
+  console.log('Seattle people are big coffee drinkers and icecream eaters.');
+}
 // if(first condition) {
 //   do this;
 // } else if(second condition) {
